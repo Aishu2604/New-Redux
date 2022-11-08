2ab6dc47286e7bb7f724587712925f3c433c7f77
Aishu2604 <aishwary.nigam04@gmail.com>
2022-11-09 02:48:36 +0530

    Subscribing and Dispatching

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,26 @@
 import { createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "incrementby5") {
+    return {
+      counter: state.counter + 5,
+    };
+  }
+
+  if (action.type === "decrementby5") {
+    return {
+      counter: state.counter - 5,
+    };
+  }
+
   if (action.type === "increment") {
     return {
-      counter: state.counter + 2,
+      counter: state.counter + 1,
     };
   }
   if (action.type === "decrement") {
     return {
-      counter: state.counter - 2,
+      counter: state.counter - 1,
     };
   }
   return state;
